dc176c4b51d995da5a2ba6487e1781daace4ede7
PaulaStael <paula.stael@estudante.ufjf.br>
2025-06-16 12:20:35 -0300

    Exercicio1e2

--- inc/fsm.h
+++ inc/fsm.h
@@ -14,7 +14,13 @@
 #define TIME_STARTUP        5U        // Tempo de inicialização (em ciclos de FSM)
 #define TIME_RECOVERY       10U       // Tempo de recuperação (em ciclos de FSM)
 #define TIME_DELAY_US       1000000U  // Atraso de cada ciclo da FSM em microssegundos (1 segundo)
+static unsigned int fault_counter = 0;
 
+//EXERCICIO
+#define TIME_PISCA          125000
+#define CICLO_TIME  5U //exercicio
+#define LED_GPIO_PIN_2       31U   //azul
+#define LED_GPIO_PIN_1       34U   //vermelho
 
 // --- Enumeração Pública para Estados do Conversor ---
 typedef enum
